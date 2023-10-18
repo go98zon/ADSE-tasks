b055ec79024a22a7db70509026067366763383a7
go98zon <148287973+go98zon@users.noreply.github.com>
2023-10-18 08:37:46 +0200

    Add files via upload

---
+++ exercise 1/task1.py
@@ -0,0 +1,32 @@
+"""
+Goal of Task 1:
+    Implement a function that reverses the word order of a string.
+"""
+
+
+def reverse(sentence):
+    """
+    input:
+        sentence (type: str)
+
+    output:
+        reversed_sentence (type: str)
+    """
+
+    # Task:
+    # ToDo: Compute the reversed word order of a string.
+    #       The usage of python packages is not allowed for this task.
+    ########################
+    #  Start of your code  #
+    ########################
+    word = sentence.split()
+    reversed_sentence = ' '.join(reversed(word))
+    ########################
+    #   End of your code   #
+    ########################
+
+    return reversed_sentence
+
+
+if __name__ == "__main__":
+    assert reverse("this is a test") == "test a is this"
